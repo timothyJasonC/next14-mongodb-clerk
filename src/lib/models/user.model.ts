164a1490e14efd7485ea7ac7f90f2fe11c37311b
Timothy Jason <timothycaviezel@gmail.com>
2024-04-27 22:03:45 +0700

    seccond commit

--- src/lib/models/user.model.ts
+++ src/lib/models/user.model.ts
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerId: {type:String, required: true, unique: true},
+    clerkId: {type:String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     username: {type: String, unique: true},
     photo: {type: String, required: true},
